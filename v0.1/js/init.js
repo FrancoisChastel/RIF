29959ea1cb45e6fa813fff6b06861980807df964
Chastel <Francois@MacBook-Pro-de-Francois-2.local>
2016-04-11 11:09:31 +0200

    Website Add - V0.1
    Adding static website

---
+++ v0.1/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+      $('.scrollspy').scrollSpy();
+      $('select').material_select();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
